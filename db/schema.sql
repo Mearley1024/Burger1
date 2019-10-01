df477f9eb884737dc9c39ed68f52c484aaabe1dd
Marcela Earley <Marcela.earley1024@gmail.com>
2019-10-01 14:18:57 -0700

    update files

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,21 @@
-devoured boolean
+### Schema
+CREATE DATABASE pets_db;
+USE pets_db;
 
--- Drops the programming_db if it already exists --
-DROP DATABASE IF EXISTS burger_db;
-
--- Created the DB "wizard_schools_db" (only works on local connections)
-CREATE DATABASE burger_db;
-
--- Use the DB wizard_schools_db for all the rest of the script
-USE burger_db;
-
--- Created the table "schools"
-CREATE TABLE burger_names (
-  id int AUTO_INCREMENT NOT NULL,
-  name varchar(30) NOT NULL,
-  PRIMARY KEY(id)
+CREATE TABLE buyers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	buyer_name varchar(255) NOT NULL,
+	PRIMARY KEY (id)
 );
 
+CREATE TABLE pets
+(
+	id int NOT NULL AUTO_INCREMENT,
+	animal_breed varchar(255) NOT NULL,
+	animal_name varchar(255) NOT NULL,
+	price int NOT NULL,
+	buyer_id int NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (buyer_id) REFERENCES buyers(id)
+);
